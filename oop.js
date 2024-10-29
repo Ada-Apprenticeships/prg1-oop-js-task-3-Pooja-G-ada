674f76ebf7b262c0e30b857aa352321893d53608
Pooja Gurung <pooja.gurung@ada.ac.uk>
2024-10-29 15:03:24 +0000

    refactoredx1

--- oop.js
+++ oop.js
@@ -1,51 +1,31 @@
 PRIORITY = { "LOW": 1, "MEDIUM": 3, "HIGH": 5, "URGENT": 7 };
-// console.log(PRIORITY["LOW"]);
 
-function validInteger (value) { // value can be a string or a number (integer)
+// Function to check if a value is a valid integer using regular expression
+function validInteger (value) { 
   const regexInteger = /^\d+$/;
   return (regexInteger.test(value))
 }  
 
-// test validInteger
-// console.log(validInteger ( '10' ) )// returns True
-// console.log(validInteger ( 10 )) // returns True 
-// console.log(validInteger ( '-10' )) // returns False
-// console.log(validInteger ( -10 ))// returns False
-// console.log(validInteger ( 0.0 ))// returns False
-// console.log(validInteger ( 10.0 ))// returns False
-// console.log(validInteger ( 10.3 ))// returns False
-// console.log(validInteger ( -10.0 )) // returns False
-
-
-function validatePriority(priority) { // value can be a string or a number (integer)
+// Function to validate and normalize a priority value
+function validatePriority(priority) { 
   const validPriorties = [1, 3, 5, 7];
   priority = parseInt(priority)
   return (validPriorties.includes(priority)) ? priority : 1
 }
 
-// test validPriorty
-// console.log(validatePriority ( 0 )) // returns 1
-// console.log(validatePriority ( 1 )) // returns 1
-// console.log(validatePriority ( 'A' )) // returns 1
-// console.log(validatePriority ( '7' )) // returns 7
-// console.log(validatePriority ( '10' )) // returns 1
-
-
-
+// Function to get the formatted date string for today
 function todaysDate () {
   const todaysDate = new Date();
-  const todaysYear = todaysDate.getFullYear();
-  const todaysMonth = todaysDate.getMonth() +1;
-  const todaysDay = todaysDate.getDate();
+  const todaysYear = String(todaysDate.getFullYear()).padStart(2, "0");
+  const todaysMonth = String(todaysDate.getMonth() +1).padStart(2, "0");
+  const todaysDay = String(todaysDate.getDate()).padStart(2, "0");
   const todaysHours = String(todaysDate.getHours()).padStart(2, "0");
   const todaysMinutes = String(todaysDate.getMinutes()).padStart(2, "0");
   const todaysSeconds = String(todaysDate.getSeconds()).padStart(2, "0");
   return `${todaysDay}/${todaysMonth}/${todaysYear} ${todaysHours}:${todaysMinutes}:${todaysSeconds}`
 }
 
-// test todaysDate
-// console.log(todaysDate());
-
+// Class representing a single task with properties: added date, title, and priority
 class Task  {
   _added;
   _title;
@@ -55,7 +35,7 @@ class Task  {
     this._title = title;
     this._priority = validatePriority(priority);
   }
-  // (title; priority)
+
   get added(){
     return this._added;
   }
@@ -73,18 +53,7 @@ class Task  {
   }
 }
 
-
-// test Task 
-// task = new Task('Get Cappuccino', PRIORITY['MEDIUM'])  // Creates an instance of a Task (named task)
-// console.log(task.added)//-> '30/4/2023 12:26:26' // Checking the 'added' attribute of a Task instance returns the date/time it was added.
-// console.log(task.title) //-> 'Get Cappuccino' // Checking the 'title' attribute for a Task instance returns the title of the task.'
-// console.log(task.priority) //-> 3 // Checking the 'priority' attribute for a Task instance returns an integer 3 (Remember MEDIUM == 3).
-// task.priority = PRIORITY['URGENT'] // Setting the 'priority' attribute for a Task instance to URGENT (Remember URGENT == 7).
-// task.priority //-> 7 // Checking the 'priority' attribute for a Task instance returns an integer 7 (Remember URGENT == 7).
-// task.priority = '10' // Setting the 'priority' attribute for a Task instance to the string '10' (an invalid priority).
-// task.priority //-> 1 // Checking the 'priority attribute for a Task instance returns an integer 1 (because '10' was an invalid priority so it defaults to 1).
-
-
+// Class ToDo representing a task management system (array) with functionalities for adding, removing, getting specific task, and listing the tasks
 class ToDo {
   _tasks;
 
@@ -97,6 +66,7 @@ class ToDo {
     return this._tasks.length;
   }
 
+  // Removes a task from the list by title and returns true if successful, false otherwise
   remove(title){
     const foundTask = this._tasks.find(task => task.title === title)
     if (foundTask !== undefined){
@@ -106,6 +76,7 @@ class ToDo {
     return false;
   }
 
+  // Returns a list of tasks (filtered by priority) with formatted data [task.added, task.title, task.priority]
   list(priority = 0){
     let mappedTasksArray = this._tasks.map(task => [task.added, task.title, task.priority]);
     if (priority !== 0){
@@ -114,61 +85,14 @@ class ToDo {
     return mappedTasksArray;
   }
 
+  // Retrieves a specific task by title and returns it or throws an error if not found
   task(title){
     const foundTask = this._tasks.find(task => task.title === title);
-    if (foundTask) {
-      return foundTask
-    }
+    if (foundTask) return foundTask
     throw new Error(`Task '${title}' Not Found`)
   }
 
 }
-
-// test ToDo
-// taskList = new ToDo () // Creates an instance of ToDo named taskList
-// taskList.add (new Task( 'Get Cappuccino', PRIORITY [ 'HIGH' ]) ) //-> 1  // Adds a new Task instance to the taskList instance and returns the number of tasks in the list: (1) in this case.
-// taskList.add (new Task( 'Order Lunch', PRIORITY [ 'MEDIUM' ]) ) //-> 2 // Adds a second Task instance to the taskList instance and returns the number of tasks in the list: (2) now.
-// taskList.add (new Task( 'Complete Project Sprint', PRIORITY [ 'MEDIUM' ]) ) //-> 3 // Adds a third Task instance to the taskList instance and returns the number of tasks in the list: (3) now. 
-// // console.log(taskList.list())
-// // taskList.list (PRIORITY [ 'MEDIUM' ] ) //->  // Calls the list method of the taskList instance and a list of lists is returned with all the MEDIUM priority tasks.
-// // console.log(taskList.list ( PRIORITY['MEDIUM'] )) 
-// // [
-//   //   [ '28/3/2023 10:16:56', 'Order Lunch', 3 ], 
-//   //   [ '28/3/2023 10:16:56', 'Complete Project Sprint', 3 ]
-//   // ]
-// // taskList.list ( PRIORITY['HIGH'] ) //->  //  Calls the list method of the taskList instance and a list of lists is returned with all the HIGH priority tasks.
-// // console.log(taskList.list ( PRIORITY['HIGH'] )) 
-// // [
-//   //   [ '28/3/2023 10:16:56', 'Get Cappuccino', 5 ]
-//   // ]
-// // taskList.task ('Complete Project Sprint').priority = PRIORITY['HIGH'] // Changes a specific task to have a HIGH priority.
-// // taskList.list ( PRIORITY['HIGH'] ) // Calls the list method of the taskList instance and a list of lists is returned with all the HIGH priority tasks.
-// // console.log(taskList.list ( PRIORITY['HIGH'] )) // Calls the list method of the taskList instance and a list of lists is returned with all the HIGH priority tasks.
-// // [
-//   //   [ '28/3/2023 10:16:56', 'Get Cappuccino', 5 ], 
-//   //   [ '28/3/2023 10:16:56', 'Complete Project Sprint', 5 ]
-//   // ]
-// // taskList.list ( PRIORITY['HIGH'] ) [0] [1] //-> 'Get Cappuccino'   // Calls the list method of the taskList instance and accesses the [0][1] position of the object, returning the task name.
-// taskList.remove ( 'Complete Project Sprint1' ) //-> false  // Calls the remove method and 'false' is returned, as a task with this name doesn't exist.
-// taskList.remove ( 'Complete Project Sprint' ) //-> true // Calls the remove method and 'true' is returned, as a task with this name DOES exist.
-// // taskList.list ( PRIORITY['HIGH'] ) //-> //  Calls the list method of the taskList instance and a list of lists is returned with all the HIGH priority tasks. (Only one now)
-//   // [
-//   //   [ '28/3/2023 10:16:56', 'Get Cappuccino', 5 ], 
-//   // ]
-// // taskList.task ( 'WrongTitle' ) //-> // throw error <Task 'WrongTitle' Not Found> as task with this name doesn't exist.
-
-const taskList = new ToDo() // creates an instance of a ToDo() object name taskList
-taskList.add(new Task ('Get Pasta', PRIORITY ['MEDIUM'])) // returns 1 as 1 task in list
-taskList.add (new Task ('Get Breakfast Cereal', PRIORITY ['MEDIUM'] )) // returns 2 as 2 tasks in list
-// console.log(taskList.list())
-taskList.remove ('Get Breakfast Cereal') // returns true (as task exists, and then removes it)
-taskList.remove ('Get dinner') // returns true (as task exists, and then removes it)
-// console.log(taskList.list())
-
-
-
-
-
 
 
 // Leave this code here for the automated tests
